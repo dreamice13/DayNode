f9f04408f6a71f971d9e4c1f2fd306f288e895b4
Dreamice <dreamice13@foxmail.com>
2022-02-15 17:24:16 +0800

    HelloWorld

---
+++ express-demos/02基础请求方法/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request')
+})
+
+app.put('/user', (req, res) => {
+    res.send('put user')
+})
+
+app.delete('/user', (req, res) => {
+    res.send('delete user')
+})
+
+app.listen(3000, () => {
+    console.log(`Server running at http://localhost:3000`);
+})
+
